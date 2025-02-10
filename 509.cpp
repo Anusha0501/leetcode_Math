e197b01bf699ddfb7c0229e8e1f90264ccddb487
Anusha Singh <anusha.22cse@bmu.edu.in>
2025-02-10 20:16:41 +0530

    Create 509.cpp

---
+++ 509.cpp
@@ -0,0 +1,11 @@
+#509. Fibonacci Number
+
+// Recursive
+// Exponential Time Complexity: O(2^N)
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0 || n == 1) return n;
+        return fib(n-1) + fib(n-2);
+    }
+};
